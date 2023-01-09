7b4788f177808f7a88f3e69d9734a874fa6be131
ChristianIlachoque <cilachoque@unsa.edu.pe>
2023-01-09 17:42:58 -0500

    Deploy App on GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import DegreList from "./components/DegreList";
 const App = () => {
   return (
     <div className="bg-emerald-100 h-screen p-2">
-      <h1 className="md:text-center font-mono mt-2 bg-emerald-200 text-[30px] text-slate-800">My Acurance App</h1>
+      <h1 className="md:text-center font-mono mt-2 bg-emerald-200 text-[30px] text-slate-800">My Average App</h1>
       <div className="container mx-auto p-10">
         <DegreList />
         <hr />
